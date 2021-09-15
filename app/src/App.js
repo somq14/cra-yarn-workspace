323ca087e1a716307853caf9259c5cb7b6ee2511
somq14 <som.q14@gmail.com>
2021-09-15 23:45:03 +0900

    フォルダ名 package はなぜかうまくいかない

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,12 @@
 import { message } from "core";
+import { message as packageMessage } from "package";
 
 function App() {
-  return <div className="App">{message}</div>;
+  return (
+    <div className="App">
+      {message} {packageMessage}
+    </div>
+  );
 }
 
 export default App;
